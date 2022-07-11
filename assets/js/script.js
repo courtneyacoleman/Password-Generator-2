de3e82a8c44fbec41fc788e3bc809af6cafb70be
Courtney Coleman <105394516+courtneyacoleman@users.noreply.github.com>
2022-07-11 10:48:45 -0500

    it works

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,7 @@ let lowerCase = '';
 let numeric = '';
 let specialChar = '';
 let password = [];
+let final = '';
 
 // onclick event for the generate button
 document.getElementById('generate').addEventListener('click', passwordLength);
@@ -23,9 +24,21 @@ function passwordLength () {
 
 function passwordCriteria() {
     upperCase = confirm('Should the password contain upper case letters?');
+    if (upperCase === true) {
+        password.push('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z')
+    }
     lowerCase = confirm('Should the password contain lower case letters?');
+    if (lowerCase === true) {
+        password.push('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z')
+    }
     numeric = confirm('Should the password contain numbers?');
-    specialChar = confirm('Should the password contain special characters?');   
+    if (numeric === true) {
+        password.push('0', '1', '2', '3', '4', '5', '6', '7', '8', '9')
+    }
+    specialChar = confirm('Should the password contain special characters?');  
+    if (specialChar === true) {
+        password.push('!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', ']', '^', '_', '`', '{', '|', '}', '~')
+    }
     console.log('upperCase:', upperCase);
     console.log('lowerCase:', lowerCase);
     console.log('numeric:', numeric);
@@ -41,7 +54,29 @@ function passwordCriteria() {
     else {
         //createPassword();
     }
+    createPassword();
 }
 
-password.length = characters
-console.log('password.length' , password.length);
+console.log('password' , password);
+console.log('char length', characters.length);
+console.log('char value', characters.value);
+
+function createPassword () {
+    for (let i = 0; i < characters; i++){
+        let rando = Math.floor(Math.random() * password.length);
+        let choice= password[rando];
+        console.log('choice', choice)
+        console.log('rando', rando)
+        final = final.concat(choice);
+        //console.log('new string', newString)
+        //final.concat(newString);
+        console.log('final:', final)
+        //console.log('newPass', newPass)
+    }
+    //alert(final);
+    var showPassword = document.getElementById('password');
+    showPassword.innerText = final;
+}
+
+
+
